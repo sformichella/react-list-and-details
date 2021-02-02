700805317bf78f01b20689e1585a7e2ef6b1a10a
sformichella <sformichella@gmail.com>
2021-02-02 14:17:47 -0800

    GET some avatar characters

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 import { 
   BrowserRouter as Router,
   Switch,
   Route
-} from 'react-router-dom';
-import Home from '../Home/Home';
-import List from '../List/List';
-import Details from '../Details/Details';
+} from "react-router-dom";
+import Home from "../Home/Home";
+import List from "../List/List";
+import Details from "../Details/Details";
 
 const App = () => {
   return (
--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React from "react";
+
+const avatarAPI = "https://last-airbender-api.herokuapp.com/api/v1/";
+const characters = "characters";
 
 export default function List() {
+
+  const sample = fetch(avatarAPI + characters)
+    .then(res => res.json())
+
+
   return (
     <div>
       This is the list page.
